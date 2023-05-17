c80bf5470c0610f2a66e0056cf15cbff758bfbd8
schr-0dinger <kmkailasnath@gmail.com>
2023-05-17 12:24:57 +0530

    Added comments to main.js

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const { BrowserWindow, app } = require("electron")
 const path = require("path")
 
-require("electron-reloader")(module);
+require("electron-reloader")(module); // Electron-reloader module is handy for dynamic changes. Might crash while JS changes
 
+// Create the Intro window
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 900,
         height: 700,
-        frame: false,
-        titleBarStyle: "hiddenInset",
+        frame: false, // Remove default title bar
+        titleBarStyle: "hiddenInset", // Using custom title bar
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            preload: path.join(__dirname, "renderer.js"),
+            preload: path.join(__dirname, "renderer.js"), // renderer.js controls the changes in UI
             enableRemoteModule: true
         }
     });
 
     mainWindow.loadFile("index.html");
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools(); // Only in developer mode ; Should remove in beta and public
 };
 
 app.whenReady().then(createWindow);
